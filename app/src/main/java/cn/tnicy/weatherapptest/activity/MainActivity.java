bd9b7689e0bfe1504bc7e564e355d37cbc38e353
Tnicy <ashyoumo@gmail.com>
2019-01-25 09:54:23 +0800

    完成遍历省市县三级列表的功能

---
+++ app/src/main/java/cn/tnicy/weatherapptest/activity/MainActivity.java
@@ -0,0 +1,14 @@
+package cn.tnicy.weatherapptest.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import cn.tnicy.weatherapptest.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
